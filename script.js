6b172fcbb6df5c0884b0a57a8eb2bf5ace28e37c
roccatt <80890275+roccatt@users.noreply.github.com>
2021-03-29 17:29:18 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function overed(){
+  document.getElementById("img1").src = "http://3.bp.blogspot.com/-bMf_r22jThI/UAb6XroA8OI/AAAAAAAAAP8/7UgGXR0_M9A/s1600/10-sheep-tree.jpg"
+}
+
+function unovered(){
+  document.getElementById("img1").src = "https://previews.123rf.com/images/pellinni/pellinni1711/pellinni171100299/91013807-forest-road-among-tall-trees-with-green-foliage-beautiful-nature-scenery-in-springtime.jpg"
+}
